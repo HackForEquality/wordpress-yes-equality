00dc2ec2a25bac0bec54ee572be308e5a0492b7e
Kevin Baker <eire32kevin@gmail.com>
2015-03-28 19:18:04 +0000

    Add sizing to the wordpress options

--- index.php
+++ index.php
@@ -10,22 +10,29 @@ I'm sorry for whomever wants to maintain this code because it really is god awfu
 function insert_my_footer() {
   $yer_position = get_option('yer_position', 'top_left');
   $yer_type = get_option('yer_type', 'camo');
+  $yer_size = get_option('yer_size', 'normal');
 
   if (strpos($yer_position, 'left') !== FALSE) {
-    $x_direction = "left";
+    $x_direction = 'left';
   } else {
-    $x_direction = "right";
+    $x_direction = 'right';
   }
 
   if (strpos($yer_position, 'top') !== FALSE) {
-    $y_direction = "top";
+    $y_direction = 'top';
   } else {
-    $y_direction = "top";
+    $y_direction = 'top';
+  }
+
+  if ($yer_size == 'normal') {
+    $size = 300;
+  } else {
+    $size = 150;
   }
 
   $image_url = plugins_url('images/'. $yer_type . '_' . $yer_position . '.png', __FILE__);
 
-  echo '<a href="http://www.yesequality.ie"><img style="position: absolute; ' . $y_direction . ': 0; ' . $x_direction . ': 0; border: 0; z-index: 99999" src="' . $image_url . '" alt="We Support Marriage Equality!"></a>';
+  echo '<a href="http://www.yesequality.ie"><img style="position: absolute; ' . $y_direction . ': 0; ' . $x_direction . ': 0; border: 0; z-index: 99999" height="' . $size . '" width="' . $size . '" src="' . $image_url . '" alt="We Support Marriage Equality!"></a>';
 }
 
 add_action('wp_footer', 'insert_my_footer');
@@ -43,15 +50,17 @@ function my_plugin_menu() {
 function register_yer_settings() {
   register_setting('yer-settings-group', 'yer_position');
   register_setting('yer-settings-group', 'yer_type');
+  register_setting('yer-settings-group', 'yer_size');
 }
 
 function my_plugin_options() {
-  if ( !current_user_can( 'manage_options' ) )  {
+  if (!current_user_can('manage_options'))  {
     wp_die( __('You do not have sufficient permissions to access this page.') );
   }
 
   $yer_position = esc_attr( get_option('yer_position', 'top_left') );
   $yer_type = esc_attr( get_option('yer_type', 'camo') );
+  $yer_size = esc_attr( get_option('yer_size', 'normal') );
 
 ?>
   
@@ -66,7 +75,7 @@ function my_plugin_options() {
         <tbody>
           <tr>
             <td scope="row" align="left">
-              <label>Position (<?php echo $yer_position ?>)</label>
+              <label>Position</label>
               <select name="yer_position" id="yer-position">
                 <option value="top_left" <?php echo ($yer_position == 'top_left') ? 'selected' : '' ?>>Top Left</option>
                 <option value="top_right" <?php echo ($yer_position == 'top_right') ? 'selected' : '' ?>>Top Right</option>
@@ -77,10 +86,19 @@ function my_plugin_options() {
           </tr>
           <tr>
             <td scope="row" align="left">
-              <label>Style (<?php echo $yer_type ?>)</label>
+              <label>Style</label>
               <select name="yer_type" id="yer-type">
                 <option value="camo" <?php echo ($yer_type == 'camo') ? 'selected' : '' ?>>Colored</option>
                 <option value="white" <?php echo ($yer_type == 'white') ? 'selected' : '' ?>>White</option>
+              </select>
+            </td>
+          </tr>
+          <tr>
+            <td scope="row" align="left">
+              <label>Size</label>
+              <select name="yer_size" id="yer-size">
+                <option value="normal" <?php echo ($yer_size == 'normal') ? 'selected' : '' ?>>Normal</option>
+                <option value="small" <?php echo ($yer_size == 'small') ? 'selected' : '' ?>>Small</option>
               </select>
             </td>
           </tr>
@@ -98,10 +116,12 @@ function my_plugin_options() {
     jQuery(document).ready(function() {
       var position = "<?php echo $yer_position ?>";
       var type = "<?php echo $yer_type ?>";
+      var size = "<?php echo $yer_size ?>";
       var url = "<?php echo plugins_url('images/', __FILE__) ?>";
 
       var positionInput = jQuery("#yer-position");
       var typeInput = jQuery("#yer-type");
+      var sizeInput = jQuery("#yer-size");
 
       positionInput.change(function() {
         position = jQuery(this).val();
@@ -115,8 +135,22 @@ function my_plugin_options() {
         displayPreview();
       });
 
+      sizeInput.change(function() {
+        size = jQuery(this).val();
+
+        displayPreview();
+      });
+
       displayPreview = function() {
-        jQuery("#yer-preview").html('<img src="' + url + type + '_' + position + '.png" />');
+        console.log(size);
+        if (size == 'normal') {
+          sizeValue = 300;
+          console.log("true");
+        } else {
+          sizeValue = 150;
+        }
+
+        jQuery("#yer-preview").html('<img src="' + url + type + '_' + position + '.png" height="' + sizeValue + '" width="' + sizeValue + '" />');
       };
 
       displayPreview();
